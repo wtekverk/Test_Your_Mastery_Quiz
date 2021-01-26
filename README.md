deb266c44a2463888c3f48ee01f068233eee7f8a
Wendy Tekverk <wtekverk@gmail.com>
2021-01-26 10:02:59 -0700

    added psudo code to java script for tasks

--- README.md
+++ README.md
@@ -23,12 +23,16 @@ SO THAT I can gauge my progress compared to my peers
 GIVEN I am taking a code quiz
 WHEN I click the start button
 THEN a timer starts and I am presented with a question
+
 WHEN I answer a question
 THEN I am presented with another question
+
 WHEN I answer a question incorrectly
 THEN time is subtracted from the clock
+
 WHEN all questions are answered or the timer reaches 0
 THEN the game is over
+
 WHEN the game is over
 THEN I can save my initials and my score
 ```
@@ -88,3 +92,7 @@ You are required to submit BOTH of the following for review:
 ---
 
 © 2021 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
+
+
+
+
---
+++ script.js
@@ -0,0 +1,45 @@
+
+//start button 
+//timer 
+//questions 
+//choices
+//clock
+//game over screen
+
+//make an html with all of these components
+
+//JS 
+
+//data - questions 
+
+//variables global
+
+//functions 
+
+//event listeners
+
+
+//click start button
+    //ref start btn on DOM
+    //add onclick event
+
+//start timer
+    //ref timer on DOM
+    //use set interval
+
+//display qs and as
+    //ref DOM
+    //loop through questions 
+    //display Qs on dom
+    //loop through choices
+    //display each choice on DOM
+
+//if answer correct show next Q
+    //check answer against choice????
+
+//if incorrect -time 
+    //time = time -15
+    
+//when all qs answered GAME OVER 
+//when time === 0 game over 
+
