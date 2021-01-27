da30467b97ef924cbecb4a89e2b83c11636d1ce8
Wendy Tekverk <wtekverk@gmail.com>
2021-01-26 20:18:13 -0700

    added variable ato js and connected them to the html

--- script.js
+++ script.js
@@ -1,7 +1,54 @@
-var startBtn = document.querySelector(".start_btn")
-var timer = document.querySelector(".timeScore")
+
+var startButton = document.getElementById("start_btn");
+var timeEl = document.querySelector(".timer");
+var questionSection = document.getElementById("question-section")
+var question = document.getElementById("question");
+var choiceA = document.getElementById("choice_A");
+var choiceB = document.getElementById("choice_B");
+var choiceC = document.getElementById("choice_C");
+var choiceD = document.getElementById("choice_D");
+var answerCheck = document.getElementById("answer_check");
+var userDetails = document.getElementById("user_details");
+var userName = document.getElementById("username");
+var saveUser = document.getElementById("saveUser");
+var finalScore = document.getElementById("final_score");
+var secondsLeft = 75;
+var currentQuestionIndex = 0
+var correctAnswers = 0
+var incorrectAnswers = 0
+
+var questions = [
+    {title: "What shape is a wombats poop?",
+    choices: ["circe", "cube", "sphere", "triangle"],
+    answer: "cube"
+    },
+
+    {title: "What is the flightless bird that is also a national icon of NZ?",
+    choices: ["blue jay", "ostrich", "kiwi", "wombat"],
+    answer: "kiwi"
+    },
+
+    {title: "The lesser short-tail is what kind of animal known to crawl around the floor of the forests of NZ?",
+    choices: ["bat", "bird", "rat", "cat"],
+    answer: "bat"
+    },
+
+    {title: "If you wanted to find the oldest fossilized remains of Penguins where should you look?",
+    choices: ["Africa", "Canada", "New Zealand", "Mexico"],
+    answer: "New Zealand"
+    },
+
+    {title: "The giant Moa is a what?",
+    choices: ["type of blue whale", "extinct bird", "kangaroo", "tree"],
+    answer: "cube"
+    }
+]
 
 
+
+function startQuiz () {
+
+}
 
 
 
